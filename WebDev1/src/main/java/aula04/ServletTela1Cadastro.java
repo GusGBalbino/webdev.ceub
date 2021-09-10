b0065ff6fece479e3ea1fe79ed016c99402a6e6c
Gustavo Gomes Balbino <gustavo.gbalbino@hotmail.com>
2021-09-09 21:07:09 -0300

    AULa 05 Completa - Cadastro

--- WebDev1/src/main/java/aula04/ServletTela1Cadastro.java
+++ WebDev1/src/main/java/aula04/ServletTela1Cadastro.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ServletTela1Cadastro")
 public class ServletTela1Cadastro extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -24,33 +24,49 @@ public class ServletTela1Cadastro extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	doPost(request, response);
+    }
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter resposta = response.getWriter();
-		
-		resposta.write("<html>");
-		resposta.write("<head>");
-		resposta.write("<title>Dados Profissionais</title>");
-		resposta.write("</head>");
-		resposta.write("<body>");
-		resposta.write("<Preencha seus dados profissionais:>");
-		resposta.write("<form action=\"ServletTela2Cadastro\">");
-		resposta.write("<Empresa:<input type=\"text\"name\"empresa\"> <BR>");
-		resposta.write("Endereço profissional: <BR>");
-		resposta.write("<html>");
-		resposta.write("<Rua:<input type=\"text\"name=\"ruaEmpresa\">");
-		resposta.write("<Cidade:<input type=\"text\"name=\"cidadeEmpresa\">");
-		resposta.write("<Estado:<input type=\"text\"name=\"estadoEmpresa\"> <BR>");
-		resposta.write("<CEP:<input type=\"text\"name=\"cepEmpresa\"> <BR>");
-		resposta.write("<Complemento:<input type=\"text\"name=\"compEmpresa\"> <BR>");
-		resposta.write("<input type=\"submit\" value=\"Confirmar\"><BR>");
-		resposta.write("</form>");
-		
-	}
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	PrintWriter resposta = response.getWriter();
+
+    	resposta.write("<html>");
+        resposta.write("<head>");
+        resposta.write("<title>Dados Profissionais</title>");
+        resposta.write("</head>");
+        resposta.write("<body>");
+        resposta.write("Preencha seus dados profissionais:");
+        resposta.write("<form action=\"ServletTela2Cadastro\">");
+
+        resposta.write("<input type=\"hidden\" name=\"nome\" value=\"");
+        resposta.write(request.getParameter("nome") + "\">");
+        resposta.write("<input type=\"hidden\" name=\"sobrenome\" value=\"");
+        resposta.write(request.getParameter("sobrenome") + "\">");
+        resposta.write("<input type=\"hidden\" name=\"rua\" value=\"");
+        resposta.write(request.getParameter("rua") + "\">");
+        resposta.write("<input type=\"hidden\" name=\"complemento\" value=\"");
+        resposta.write(request.getParameter("complemento") + "\">");
+        resposta.write("<input type=\"hidden\" name=\"cidade\" value=\"");
+        resposta.write(request.getParameter("cidade") + "\">");
+        resposta.write("<input type=\"hidden\" name=\"cep\" value=\"");
+        resposta.write(request.getParameter("cep") + "\">");
+        resposta.write("<input type=\"hidden\" name=\"estado\" value=\"");
+        resposta.write(request.getParameter("estado") + "\">");
+
+        resposta.write("Empresa:<input type=\"text\" name=\"empresa\"> <BR>");
+        resposta.write("Endereço profissional:<BR>");
+        resposta.write("Rua: <input type=\"text\" name=\"ruaEmpresa\">");
+        resposta.write("Complemento:<input type=\"text\" name=\"complementoEmpresa\"><BR>");
+        resposta.write("Cidade:<input type=\"text\" name=\"cidadeEmpresa\">");
+        resposta.write("CEP:<input type=\"text\" name=\"cepEmpresa\">");
+        resposta.write("Estado:<input type=\"text\" name=\"estadoEmpresa\"><BR>");
+        resposta.write("<input type=\"submit\" value=\"Confirmar\"><BR>");
+        resposta.write("</form>");
+        resposta.write("</body></html>");
+
+    }
 
 }
